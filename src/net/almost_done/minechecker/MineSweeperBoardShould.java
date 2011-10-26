8296685df424c81c787443afe6bf3e14b35d4ab7
Jacek Królikowski <nietaki@gmail.com>
2011-10-26 20:12:28 +0200

    more tests - mine out of bounds

--- src/net/almost_done/minechecker/MineSweeperBoardShould.java
+++ src/net/almost_done/minechecker/MineSweeperBoardShould.java
@@ -10,8 +10,20 @@ public class MineSweeperBoardShould {
 	public void acceptAValidMine() throws Exception {
 		MineSweeperBoard mb = new MineSweeperBoard(8, 8);
 		assertTrue(mb.placeMine(1, 2));
-		
 	}
+
+	@Test
+	public void notAcceptARepeatedMine() throws Exception {
+		MineSweeperBoard mb = new MineSweeperBoard(8, 8);
+		mb.placeMine(1, 2);
+		assertFalse(mb.placeMine(1, 2));
+	}
+
+	@Test
+	public void notAcceptAMineOutOfBoundsX1() throws Exception {
+		MineSweeperBoard mb = new MineSweeperBoard(8, 8);
+		assertFalse(mb.placeMine(-1, 2));
+	}	
 	
 	
 }
